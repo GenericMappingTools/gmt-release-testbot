fe7736400a374506a54a9214b65b1d010a8a66ca
Dongdong Tian <seisman.info@gmail.com>
2019-11-15 10:19:33 -0500

    Test GMT Macports install on 10.13 and 10.14 (#3)

---
+++ install-gmt-via-macports.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -x
+
+# Install macports
+wget --quiet https://distfiles.macports.org/MacPorts/MacPorts-2.6.2.tar.bz2
+tar xzf MacPorts-2.6.2.tar.bz2
+cd MacPorts-2.6.2
+./configure && make && sudo make install
+export PATH=/opt/local/bin:${PATH}
+sudo port -v selfupdate
+
+# Install GMT
+sudo port install gdal +hdf5 +netcdf +openjpeg
+sudo port install gmt6
+sudo port install graphicsmagick ffmpeg
+
+set +x
