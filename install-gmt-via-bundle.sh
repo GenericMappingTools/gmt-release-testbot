338d94dbcf5b78c0844aa35f9e20a597fd2220b5
Dongdong Tian <seisman.info@gmail.com>
2019-11-14 22:43:08 -0500

    Fix bundle install script

--- install-gmt-via-bundle.sh
+++ install-gmt-via-bundle.sh
@@ -1,28 +1,18 @@
 #!/usr/bin/env bash
 
-set -x
+set -x -e
+GMT_BUNDLE="http://mirrors.ustc.edu.cn/gmt/bin/gmt-6.0.0-darwin-x86_64.dmg"
+GMT_PACKAGE_FULLNAME=$(basename ${GMT_BUNDLE})
+GMT_PACKAGE_NAME=${GMT_PACKAGE_FULL_NAME%.*}
+GMT_VERSION=$(echo ${GMT_PACKAGE_NAME} | cut -d- -f2)
 
-if [[ "${TRAVIS_JOB_NAME}" =~ ^Bundle ]]; then
-	GMT_PACKAGE_NAME=gmt-${GMT_VERSION}-darwin-x86_64
-	GMT_PACKAGE_SUFFIX=dmg
-	GMT_PACKAGE_URL=${GMT_SITE}/${GMT_PACKAGE_NAME}.${GMT_PACKAGE_SUFFIX}
+wget --quiet -c ${GMT_BUNDLE}
+sudo hdiutil attach ${GMT_PACKAGE_FULLNAME}
+cp -rf /Volumes/${GMT_PACKAGE_NAME}/GMT-${GMT_VERSION}.app /Applications
+sudo hdiutil unmount /Volumes/${GMT_PACKAGE_NAME}
 
-	# Download
-	wget --quiet ${GMT_PACKAGE_URL}
-
-	# Install
-	sudo hdiutil attach ${GMT_PACKAGE_NAME}.${GMT_PACKAGE_SUFFIX}
-	cp -rf /Volumes/${GMT_PACKAGE_NAME}/GMT-${GMT_VERSION}.app /Applications
-	sudo hdiutil unmount /Volumes/${GMT_PACKAGE_NAME}
-
-	export GMTHOME=/Applications/GMT-${GMT_VERSION}.app/Contents/Resources
-	export PROJ_LIB=${GMTHOME}/share/proj6
-	export MAGICK_CONFIGURE_PATH=$GMTHOME/lib/GraphicsMagick-1.3.33/config
-	export PATH=${GMTHOME}/bin:${PATH}
-elif [[ "${TRAVIS_JOB_NAME}" =~ ^Homebrew ]]; then
-	brew update
-	brew install gmt
-	brew install ghostscript graphicsmagick ffmpeg
-fi
-
-set +x
+export GMTHOME=/Applications/GMT-${GMT_VERSION}.app/Contents/Resources
+export PROJ_LIB=${GMTHOME}/share/proj6
+export MAGICK_CONFIGURE_PATH=$GMTHOME/lib/GraphicsMagick-1.3.33/config
+export PATH=${GMTHOME}/bin:${PATH}
+set +x +e
