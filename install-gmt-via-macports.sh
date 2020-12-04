4d66b66076e7996eb61dad06a4c15195bf4e4167
Dongdong Tian <seisman.info@gmail.com>
2020-12-04 02:15:46 -0500

    Improve the script for macports (#22)

--- install-gmt-via-macports.sh
+++ install-gmt-via-macports.sh
@@ -6,10 +6,18 @@
 set -x -e
 
 # Install macports
-wget --quiet https://distfiles.macports.org/MacPorts/MacPorts-2.6.4.tar.bz2
-tar xzf MacPorts-2.6.4.tar.bz2
-cd MacPorts-2.6.4
-./configure && make && sudo make install
+macos_version=$(sw_vers -productVersion)
+if [[ "$macos_version" =~ "11.0" ]]; then
+	pkg="https://distfiles.macports.org/MacPorts/MacPorts-2.6.4_1-11-BigSur.pkg"
+elif [[ "$macos_version" =~ "10.15" ]]; then
+	pkg="https://distfiles.macports.org/MacPorts/MacPorts-2.6.4-10.15-Catalina.pkg"
+else
+	echo "macOS version ${macos_version} not supported."
+	exit 1
+fi
+wget --quiet $pkg
+
+sudo installer -verbose -pkg $(basename $pkg) -target /
 export PATH=/opt/local/bin:${PATH}
 sudo port -v selfupdate
 
