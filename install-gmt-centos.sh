789e682b88b70c568cfc764e73ba5a66626ffc38
Dongdong Tian <seisman.info@gmail.com>
2020-12-04 16:29:49 -0500

    Further improvement

---
+++ install-gmt-centos.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# Install GMT on CentOS
+#
+
+set -x -e
+yum -yq install epel-release
+yum -yq install yum-plugin-copr
+yum -yq copr enable genericmappingtools/gmt
+
+# See https://github.com/GenericMappingTools/gmt/issues/3433
+if [ "$(rpm -E %rhel)" == "8" ]; then
+    yum config-manager --set-enabled PowerTools
+fi
+
+yum -yq install gmt
+yum -yq localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm
+yum -yq install GraphicsMagick ffmpeg gdal
+
+set +x +e
